b660255938a6548eff11cf0cd14dfba516fb8c70
Shivam Khokhani <sxk2125@mavs.uta.edu>
2026-04-04 17:03:23 -0500

    Initial backend setup

---
+++ backend/src/app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/api/health", (req, res) => {
+    res.json({message : "Backend is running!"});
+})
+
+module.exports = app;
+
+
